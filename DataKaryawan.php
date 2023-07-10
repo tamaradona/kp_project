adb19809e88189df3136633a0f53958dc763dee4
Tamara Dwi R <100558933+tamaradona@users.noreply.github.com>
2023-07-10 08:34:03 +0700

    DataKaryawan.php

--- DataKaryawan.php
+++ DataKaryawan.php
@@ -5,8 +5,17 @@ $query = mysqli_query($mysqli, "SELECT * from db_karyawan1");
 
 
 ?>
-<td><a href="tambah data.php"class="btn btn-sm btn-info"style "center">Tambah Karyawan</a>
+<center>
+<form method="GET" action="DataKaryawan.php" >
+    
+    <label>pencarian kata kunci</label>
+    <input type="text" name="cari" value="<?php if(isset($_GET['cari'])){echo $_GET['cari'];}?>">
+    <button type="submit">cari</button>
+    <br>
+</form>
+<td><a href="tambah data.php"class="btn btn-sm btn-info"<left>Tambah Karyawan</a>
 <table class="table-bordered table-responsive"width="100%" >
+</td>
     <thead>
         <tr>
             <th>No</th><th>NIP</th><th>Nama</th><th>Golongan</th><th>Seksi</th><th>Jenis Kelamin</th><th>Jabatan Statistik</th><th>Username</th>
@@ -14,6 +23,7 @@ $query = mysqli_query($mysqli, "SELECT * from db_karyawan1");
     </thead>
     <tbody>
         <?php
+        
         $no=0;
         while($result = mysqli_fetch_array($query)){
             $no++;
@@ -29,9 +39,10 @@ $query = mysqli_query($mysqli, "SELECT * from db_karyawan1");
             <td><?php echo $result['username'];?></td>
             <td>
                 <a href="hapus data.php?no=<?php echo $result['no'];?>" class="btn btn-sm btn-danger">Hapus</a>
-                <a href="form edit.php?no=<?php echo $result['no'];?>&&page=edit" class="btn btn-sm btn-warning">Edit</a>
+                <a href="home.php?no=<?php echo $result['no'];?>&&page=edit" class="btn btn-sm btn-warning">Edit</a>
             </td>
         </tr>
         <?php }?>
 </tbody>
 </table>
+</center>
