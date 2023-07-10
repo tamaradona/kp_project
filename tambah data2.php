d170cec47e4e2481c0732f3dc930e3cdbc980810
Tamara Dwi R <100558933+tamaradona@users.noreply.github.com>
2023-07-10 08:48:03 +0700

    tambah data2.php

--- tambah data2.php
+++ tambah data2.php
@@ -2,7 +2,7 @@
 include '../auth/koneksi2.php';
 
 $query = mysqli_query($mysqli, "INSERT INTO `db_mitra`(`no`, `nik`, `nama`, `provinsi`, `kabupaten/kota`, `kec`, `desa/kel`, `jenis_kelamin`, `no_telp`) 
-VALUES ('[]','[]','[]','[]','[]','[]','[]','[]','[]')");
+VALUES ('','','','','','','','','')");
 header('location:DataMitra.php');
 ?>
 
