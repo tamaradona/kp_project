c0e19851eb05bd9781888a02f8a1babb472f3a2e
Tamara Dwi R <100558933+tamaradona@users.noreply.github.com>
2023-07-10 08:42:02 +0700

    form edit.php

--- form edit.php
+++ form edit.php
@@ -1,52 +1,41 @@
-<head>
-  	<title>Edit Karyawan</title>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900" rel="stylesheet">
-		
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-		<link rel="stylesheet" href="css/style.css">
-    </head><form>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+<h2>Form Edit Data Karyawan</h2>
+<form action="edit data.php?no=<?php echo $_GET['no'];?>" method="post">
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputEmail4">NIP</label>
-      <input type="text" class="form-control" id="inputEmail4" placeholder="nip" name="nip" required>
+      <input type="text" class="form-control" id="inputNIP" placeholder="nip" name="nip" required>
     </div>
   <div class="form-group">
     <label for="inputAddress">nama</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="" name="nama" required>
+    <input type="text" class="form-control" id="inputNama" placeholder="" name="nama" required>
   </div>
   </div>
   <div class="form-group">
     <label for="inputAddress">Golongan</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="golongan" name="golongan" required>
+    <input type="text" class="form-control" id="inputGolongan" placeholder="golongan" name="golongan" required>
   </div>
   <div class="form-group">
     <label for="inputAddress2">Seksi</label>
-    <input type="text" class="form-control" id="inputAddress2" placeholder="seksi" name="seksi" required>
+    <input type="text" class="form-control" id="inputSeksi" placeholder="seksi" name="seksi" required>
   </div>
   <div class="form-row">
     <div class="form-group col-md-6">
       <label for="inputCity">Jenis Kelamin</label>
-      <input type="text" class="form-control" id="inputCity" placeholder="jenis kelamin" name="jenis kelamin" required>
+      <input type="text" class="form-control" id="inputJenisKelamin" placeholder="jenis kelamin" name="jenis kelamin" required>
     </div>
   </div>
   <div class="form-group">
     <label for="inputAddress">Jabatan Statistik</label>
-    <input type="text" class="form-control" id="inputAddress" placeholder="" name="jabatan statistik" required>
+    <input type="text" class="form-control" id="inputJabatanStatistik" name="jabatan statistik" required>
   </div>
   <div class="form-row">
   <div class="form-group col-md-6">
       <label for="inputCity2">Username</label>
-      <input type="text" class="form-control" id="inputCity2" placeholder="username" name="username" required>
+      <input type="text" class="form-control" id="inputUsername" placeholder="username" name="username" required>
   </div>
 </div>
-  <button type="submit" class="btn btn-primary">Simpan</button>
 
+  <button type="submit" class="btn btn-primary">Simpan</button>
 </form>
 
-<script src="js/jquery.min.js"></script>
-    <script src="js/popper.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
